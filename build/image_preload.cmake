fca1d7754aef330ff05a0255393278b271895ca4
mkuettner97 <udotf@student.kit.edu>
2023-12-06 10:48:37 +0100

    implement stereo le audio, i2c, sensor reading, battery service, device info service, rgb led service

--- build/image_preload.cmake
+++
@@ -1,7 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf5340_audio_dk_nrf5340_cpuapp" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "C:/ncs/v2.5.0/nrf/applications/nrf5340_audio/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "C:/ncs/v2.5.0/nrf/applications/nrf5340_audio/nrf5340_audio_dk_nrf5340_cpuapp.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/c57af46cb7/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
