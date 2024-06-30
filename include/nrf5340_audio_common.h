676c8893c20d0dcd748c816e33e8f167b7299f30
mkuettner97 <udotf@student.kit.edu>
2024-06-30 20:20:35 +0200

    battery service extension to v1.1

--- include/nrf5340_audio_common.h
+++ include/nrf5340_audio_common.h
@@ -8,12 +8,24 @@
 #define _NRF5340_AUDIO_COMMON_H_
 
 #include <zephyr/bluetooth/audio/audio.h>
+#include <zephyr/drivers/gpio.h>
+
+#include <zephyr/pm/device.h>
+#include <zephyr/pm/device_runtime.h>
+
+#include "LoadSwitchPM.h"
 
 #define ZBUS_READ_TIMEOUT_MS	K_MSEC(100)
 #define ZBUS_ADD_OBS_TIMEOUT_MS K_MSEC(200)
 
 #define SENSOR_DATA_FIXED_LENGTH 9
 
+#define load_switch_1_8_id DT_NODELABEL(load_switch)
+#define load_switch_3_3_id DT_NODELABEL(bq25120a)
+
+extern const struct device *const cons;
+extern const struct device *const ls_1_8;
+extern const struct device *const ls_3_3;
 
 enum sensor_id {
 	ID_IMU=0,
@@ -25,7 +37,7 @@ enum sensor_id {
 
 struct battery_data {
     uint8_t battery_level;
-    uint8_t charging_state;
+    uint16_t charging_state;
 };
 
 struct sensor_data {
@@ -34,13 +46,13 @@ struct sensor_data {
     uint32_t time;
     float data[SENSOR_DATA_FIXED_LENGTH];
     //uint8_t * data;
-};
+} __attribute__((packed));
 
 struct sensor_config {
     uint8_t sensorId;
     float sampleRate;
     uint32_t latency;
-};
+} __attribute__((packed));
 
 /***** Messages for zbus ******/
 
--- src/bluetooth/gatt_services/battery_service.h
+++ src/bluetooth/gatt_services/battery_service.h
@@ -6,13 +6,33 @@
 #include <zephyr/bluetooth/gatt.h>
 #include "nrf5340_audio_common.h"
 
+// as defined in GATT Specification Supplement
+struct battery_level_status {
+	uint8_t flags;
+	uint16_t power_state;
+	/*uint16_t identifier;
+	uint8_t battery_level;
+	uint8_t additional_status;*/
+} __attribute__((packed));
+
+// Battery Energy Status
+struct battery_energy_status {
+	uint8_t flags;
+	//uint16_t external_power_source;
+    __fp16 voltage;
+	//__fp16 available_energy;
+	__fp16 available_capacity;
+	__fp16 charge_rate;
+	//__fp16 available_energy_last;
+} __attribute__((packed));
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 int init_battery_service();
 
-float get_battery_level();
+//float get_battery_level();
 
 int bt_send_battery_level(struct battery_data * data);
 
