cc5971938c1f70d101dab98a116b735a15878dd0
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2024-11-04 16:13:19 +0100

    fixed naming of IMU components to be compatible with OpenEarable v1

--- src/ParseInfo/DefaultSensors.h
+++ src/ParseInfo/DefaultSensors.h
@@ -3,38 +3,38 @@
 
 #include "SensorScheme.h"
 
-// Define components for each group
+// ============= IMU =============
+
 SensorComponent accComponents[] = {
-    { .name = "ACC_X", .unit = "g", .parseType = PARSE_TYPE_FLOAT },
-    { .name = "ACC_Y", .unit = "g", .parseType = PARSE_TYPE_FLOAT },
-    { .name = "ACC_Z", .unit = "g", .parseType = PARSE_TYPE_FLOAT },
+    { .name = "X", .unit = "g", .parseType = PARSE_TYPE_FLOAT },
+    { .name = "Y", .unit = "g", .parseType = PARSE_TYPE_FLOAT },
+    { .name = "Z", .unit = "g", .parseType = PARSE_TYPE_FLOAT },
 };
 
 SensorComponent gyroComponents[] = {
-    { .name = "GYRO_X", .unit = "dps", .parseType = PARSE_TYPE_FLOAT },
-    { .name = "GYRO_Y", .unit = "dps", .parseType = PARSE_TYPE_FLOAT },
-    { .name = "GYRO_Z", .unit = "dps", .parseType = PARSE_TYPE_FLOAT },
+    { .name = "X", .unit = "dps", .parseType = PARSE_TYPE_FLOAT },
+    { .name = "Y", .unit = "dps", .parseType = PARSE_TYPE_FLOAT },
+    { .name = "Z", .unit = "dps", .parseType = PARSE_TYPE_FLOAT },
 };
 
 SensorComponent magComponents[] = {
-    { .name = "MAG_X", .unit = "uT", .parseType = PARSE_TYPE_FLOAT },
-    { .name = "MAG_Y", .unit = "uT", .parseType = PARSE_TYPE_FLOAT },
-    { .name = "MAG_Z", .unit = "uT", .parseType = PARSE_TYPE_FLOAT },
+    { .name = "X", .unit = "uT", .parseType = PARSE_TYPE_FLOAT },
+    { .name = "Y", .unit = "uT", .parseType = PARSE_TYPE_FLOAT },
+    { .name = "Z", .unit = "uT", .parseType = PARSE_TYPE_FLOAT },
 };
 
-// Define groups for the IMU sensor
 SensorComponentGroup imuGroups[] = {
     { .name = "ACC", .componentCount = 3, .components = accComponents },
     { .name = "GYRO", .componentCount = 3, .components = gyroComponents },
     { .name = "MAG", .componentCount = 3, .components = magComponents },
 };
 
-// Define the sensor scheme
+// ============= Sensors =============
+
 SensorScheme sensors[] = {
     { .name = "IMU", .id = 0, .groupCount = 3, .groups = imuGroups },
 };
 
-// Define the ParseInfoScheme instance
 ParseInfoScheme defaultSensors = {
     .sensorCount = 1,
     .sensors = sensors,
