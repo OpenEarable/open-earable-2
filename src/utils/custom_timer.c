b32f7de315fd0ceb5aa4c0f8af3d4eb7ae3a108a
TobiasRoeddiger <wasserglas@t-online.de>
2025-02-14 15:54:44 +0100

    changed logic to ticks

--- src/utils/custom_timer.c
+++ src/utils/custom_timer.c
@@ -1,7 +1,6 @@
 #include <custom_timer.h>
 #include <zephyr/kernel.h>
 
-static const uint32_t TICKS_PER_US = CONFIG_SYS_CLOCK_TICKS_PER_SEC / 1000000;
 static struct k_timer my_timer;
 
 // 64-bit counter for microseconds
@@ -10,20 +9,20 @@ static volatile uint64_t micros_counter = 0;
 // Timer expiry function to increment the counter
 void timer_expiry_function(struct k_timer *timer_id)
 {
-    micros_counter += UINT32_MAX; // Increment by the timer interval
+    micros_counter += UINT32_MAX * 1000000ULL / CONFIG_SYS_CLOCK_TICKS_PER_SEC; 
 }
 
 void custom_timer_start(void)
 {
     k_timer_init(&my_timer, timer_expiry_function, NULL);
-    k_timer_start(&my_timer, K_USEC(UINT32_MAX), K_USEC(UINT32_MAX)); // Max interval
+    k_timer_start(&my_timer, K_TICKS(UINT32_MAX), K_TICKS(UINT32_MAX)); // Max interval
 }
 
 uint64_t custom_micros(void)
 {
     // Read remaining ticks and convert to microseconds
     k_ticks_t elapsed_ticks = UINT32_MAX - k_timer_remaining_ticks(&my_timer);
-    uint64_t elapsed_us = elapsed_ticks / TICKS_PER_US;
+    uint64_t elapsed_us = elapsed_ticks * 1000000ULL / CONFIG_SYS_CLOCK_TICKS_PER_SEC;
 
     return micros_counter + elapsed_us;
 }
