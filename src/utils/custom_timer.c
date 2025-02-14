01eaabcddb80809d3a240ceb9b79e7a7be688811
TobiasRoeddiger <wasserglas@t-online.de>
2025-02-14 15:55:57 +0100

    more expressive name for timer

--- src/utils/custom_timer.c
+++ src/utils/custom_timer.c
@@ -1,7 +1,7 @@
 #include <custom_timer.h>
 #include <zephyr/kernel.h>
 
-static struct k_timer my_timer;
+static struct k_timer ticks_timer;
 
 // 64-bit counter for microseconds
 static volatile uint64_t micros_counter = 0;
@@ -14,14 +14,14 @@ void timer_expiry_function(struct k_timer *timer_id)
 
 void custom_timer_start(void)
 {
-    k_timer_init(&my_timer, timer_expiry_function, NULL);
-    k_timer_start(&my_timer, K_TICKS(UINT32_MAX), K_TICKS(UINT32_MAX)); // Max interval
+    k_timer_init(&ticks_timer, timer_expiry_function, NULL);
+    k_timer_start(&ticks_timer, K_TICKS(UINT32_MAX), K_TICKS(UINT32_MAX)); // Max interval
 }
 
 uint64_t custom_micros(void)
 {
     // Read remaining ticks and convert to microseconds
-    k_ticks_t elapsed_ticks = UINT32_MAX - k_timer_remaining_ticks(&my_timer);
+    k_ticks_t elapsed_ticks = UINT32_MAX - k_timer_remaining_ticks(&ticks_timer);
     uint64_t elapsed_us = elapsed_ticks * 1000000ULL / CONFIG_SYS_CLOCK_TICKS_PER_SEC;
 
     return micros_counter + elapsed_us;
