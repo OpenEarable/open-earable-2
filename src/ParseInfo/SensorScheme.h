24bdffefc5733e70317a1b25dfa6c5ca6640207c
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-02-27 11:43:36 +0100

    implemented new sensor scheme

--- src/ParseInfo/SensorScheme.h
+++ src/ParseInfo/SensorScheme.h
@@ -9,20 +9,40 @@
     BT_UUID_128_ENCODE(0xcaa25cb7, 0x7e1b, 0x44f2, 0xadc9, 0xe8c06c9ced43)
 
 #define BT_UUID_PARSE_INFO_CHARAC_VAL \
-    BT_UUID_128_ENCODE(0xcaa25cb8, 0x7e1b, 0x44f2, 0xadc9, 0xe8c06c9ced43)
+    BT_UUID_128_ENCODE(0xcaa25cb9, 0x7e1b, 0x44f2, 0xadc9, 0xe8c06c9ced43)
 
 #define BT_UUID_PARSE_INFO_SERVICE       BT_UUID_DECLARE_128(BT_UUID_PARSE_INFO_SERVICE_VAL)
 #define BT_UUID_PARSE_INFO_CHARAC        BT_UUID_DECLARE_128(BT_UUID_PARSE_INFO_CHARAC_VAL)
 
+enum SensorConfigOptionsMasks {
+    DATA_STREAMING = 0x01,
+    DATA_STORAGE = 0x02,
+    FREQUENCIES_DEFINED = 0x10,
+};
+
+struct FrequencyOptions {
+    uint8_t frequencyCount;
+    uint8_t defaultFrequencyIndex;
+    uint8_t maxBleFrequencyIndex;
+    float* frequencies;
+};
+
+struct SensorConfigOptions {
+    uint8_t availableOptions;
+    FrequencyOptions frequencyOptions;
+};
+
+
 struct SensorScheme {
     std::string name;
-    int id;
-    size_t groupCount;
+    uint8_t id;
+    uint8_t groupCount;
     SensorComponentGroup* groups;
+    SensorConfigOptions configOptions;
 };
 
 struct ParseInfoScheme {
-    size_t sensorCount;
+    uint8_t sensorCount;
     SensorScheme* sensors;
 };
 
