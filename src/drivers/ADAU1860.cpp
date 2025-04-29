23b4cdb6741b164ec6234fa05e04b66241eba006
mkuettner97 <udotf@student.kit.edu>
2025-04-29 18:45:37 +0200

    fix rebase error

--- src/drivers/ADAU1860.cpp
+++ src/drivers/ADAU1860.cpp
@@ -264,8 +264,6 @@ int ADAU1860::setup_EQ() {
 
         // EQ_ROUTE - ASRCI channel 0
         uint8_t eq_route = 64; // ASRCI channel 0
-        // EQ_ROUTE - ASRCI channel 0
-        uint8_t eq_route = 64; // ASRCI channel 0
         writeReg(registers::EQ_ROUTE, &eq_route, sizeof(eq_route));
 
         // EQ_ROUTE - serial port 0 channel 0
