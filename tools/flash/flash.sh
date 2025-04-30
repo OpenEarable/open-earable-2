ba4690e1a2f5bb99be74658cab310c3b0ac08895
mkuettner97 <udotf@student.kit.edu>
2025-04-30 12:02:12 +0200

    update flash.sh to require --snr

--- tools/flash/flash.sh
+++ tools/flash/flash.sh
@@ -1,39 +1,52 @@
 #!/bin/bash
 
-# Standard-Parameter
-SNR=261010806
+# Default parameters
 CLOCKSPEED=8000
 CHIP=NRF53
 
-# Prüfen, ob --left oder --right als Argument übergeben wurde
-if [[ "$1" == "--left" ]]; then
-    LEFT=true
-elif [[ "$1" == "--right" ]]; then
-    RIGHT=true
+# Function to show usage
+show_usage() {
+    echo "Usage: $0 --snr <serial_number> [--left|--right]"
+    echo "  --snr: Device serial number (required)"
+    echo "  --left: Flash left earable configuration"
+    echo "  --right: Flash right earable configuration"
+    exit 1
+}
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --snr) SNR="$2"; shift ;;
+        --left) LEFT=true ;;
+        --right) RIGHT=true ;;
+        *) show_usage ;;
+    esac
+    shift
+done
+
+# Check if SNR is provided
+if [ -z "$SNR" ]; then
+    echo "Error: Serial number (--snr) is required"
+    show_usage
 fi
 
-# Falls weder --left noch --right angegeben wurde, Backup UICR durchführen
+# Rest of your script remains the same
 if [ -z "$LEFT" ] && [ -z "$RIGHT" ]; then
     nrfjprog --readuicr ./tools/flash/uicr_backup.hex -f $CHIP --snr $SNR --clockspeed $CLOCKSPEED
 fi
 
-# Flash CPUNET
 nrfjprog --program ./build/merged_CPUNET.hex --chiperase --verify -f $CHIP --coprocessor CP_NETWORK --snr $SNR --clockspeed $CLOCKSPEED
 
-# Flash CPUAPP
 nrfjprog --program ./build/merged.hex --chiperase --verify -f $CHIP --coprocessor CP_APPLICATION --snr $SNR --clockspeed $CLOCKSPEED
 
-# Falls weder --left noch --right angegeben wurde, UICR wiederherstellen
 if [ -z "$LEFT" ] && [ -z "$RIGHT" ]; then
     nrfjprog --program ./tools/flash/uicr_backup.hex -f $CHIP --snr $SNR --clockspeed $CLOCKSPEED --verify
 fi
 
-# Wenn --left oder --right angegeben wurde, den entsprechenden Wert setzen
 if [ "$LEFT" == true ]; then
     nrfjprog --memwr 0x00FF80F4 --val 0 -f $CHIP --snr $SNR --clockspeed $CLOCKSPEED
 elif [ "$RIGHT" == true ]; then
     nrfjprog --memwr 0x00FF80F4 --val 1 -f $CHIP --snr $SNR --clockspeed $CLOCKSPEED
 fi
 
-# Reset
 nrfjprog --reset -f $CHIP --snr $SNR --clockspeed $CLOCKSPEED
