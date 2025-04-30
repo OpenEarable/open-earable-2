6fbb44de9ad39848b17cd6ffd4ead36968b46d92
mkuettner97 <udotf@student.kit.edu>
2025-04-30 11:49:11 +0200

    add inidvidual delay for load switches

--- boards/teco/openearable_v2/board_init.c
+++ boards/teco/openearable_v2/board_init.c
@@ -14,7 +14,8 @@ LOG_MODULE_REGISTER(board_init, LOG_LEVEL_DBG);
 
 #define load_switch_sd_id DT_NODELABEL(load_switch_sd)
 #define load_switch_1_8_id DT_NODELABEL(load_switch)
-#define load_switch_3_3_id DT_NODELABEL(bq25120a)
+#define load_switch_3_3_id DT_CHILD(DT_NODELABEL(bq25120a), load_switch)
+//#define load_switch_3_3_id DT_NODELABEL(lsctrl)
 
 const struct device *const cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 const struct device *const ls_1_8 = DEVICE_DT_GET(load_switch_1_8_id);
@@ -23,6 +24,7 @@ const struct device *const ls_sd = DEVICE_DT_GET(load_switch_sd_id);
 
 struct load_switch_data {
     struct gpio_dt_spec ctrl_pin;
+    int delay_us;
     bool default_on;
 };
 
@@ -38,7 +40,7 @@ int generic_pm_control(const struct device *dev, enum pm_device_action action)
     case PM_DEVICE_ACTION_RESUME:
         /* resume the device */
         gpio_pin_set_dt(&data->ctrl_pin, 1);
-        k_usleep(1000); //LS: t_on = 250µs, LDO: 500µs
+        k_usleep(data->delay_us); //LS: t_on = 250µs, LDO: 500µs
         break;
     default:
         return -ENOTSUP;
@@ -70,16 +72,19 @@ int init_pm_device(const struct device *dev)
 static struct load_switch_data load_switch_1_8 = {
     .ctrl_pin = GPIO_DT_SPEC_GET(load_switch_1_8_id, enable_gpios),
     .default_on = DT_NODE_HAS_PROP(load_switch_1_8_id, default_on),
+    .delay_us = DT_PROP(load_switch_1_8_id, power_delay_us)
 };
 
 static struct load_switch_data load_switch_3_3 = {
-    .ctrl_pin = GPIO_DT_SPEC_GET(load_switch_3_3_id, lsctrl_gpios),
-    .default_on = DT_NODE_HAS_PROP(load_switch_3_3_id, default_on)
+    .ctrl_pin = GPIO_DT_SPEC_GET(load_switch_3_3_id, enable_gpios),
+    .default_on = DT_NODE_HAS_PROP(load_switch_3_3_id, default_on),
+    .delay_us = DT_PROP(load_switch_3_3_id, power_delay_us),
 };
 
 static struct load_switch_data load_switch_sd_d = {
     .ctrl_pin = GPIO_DT_SPEC_GET(load_switch_sd_id, enable_gpios),
     .default_on = DT_NODE_HAS_PROP(load_switch_sd_id, default_on),
+    .delay_us = DT_PROP(load_switch_sd_id, power_delay_us),
 };
 
 PM_DEVICE_DT_DEFINE(load_switch_sd_id, generic_pm_control);
