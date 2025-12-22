d3f11d77d69b6cec7d83c112de44987c42f601ac
mkuettner97 <udotf@student.kit.edu>
2025-12-22 11:37:09 +0100

    remove test tone

--- unicast_server/main.cpp
+++ unicast_server/main.cpp
@@ -123,16 +123,6 @@ int main(void) {
 
 	ret = init_time_sync();
 	ERR_CHK(ret);
-	
-	/* Wait for shell to be ready */
-	k_sleep(K_MSEC(100));
-	const struct shell *shell = shell_backend_rtt_get_ptr();
-	if (shell) {
-		/* Execute shell commands */
-		shell_execute_cmd(shell, "audio_system start");
-		k_msleep(100); /* Give audio system time to initialize */
-		shell_execute_cmd(shell, "test nrf_multitone_start 1000 1");
-	}
 
 	// error test
 	//long *a = nullptr;
