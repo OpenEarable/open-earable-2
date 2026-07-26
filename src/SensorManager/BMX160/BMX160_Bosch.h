6391de4baebf106e23c9d63685e97faf9d6b6ef4
mkuettner97 <udotf@student.kit.edu>
2026-07-26 13:01:33 +0200

    feat: integrate Bosch BMX160 FIFO driver

---
+++ src/SensorManager/BMX160/BMX160_Bosch.h
@@ -0,0 +1,75 @@
+#ifndef BMX160_BOSCH_H
+#define BMX160_BOSCH_H
+
+#include <stdint.h>
+
+#include <TWIM.h>
+
+extern "C" {
+#include "bosch/bmi160.h"
+#include "bosch/bmm150.h"
+}
+
+struct BMX160Sample {
+    float accel[3];
+    float gyro[3];
+    float mag[3];
+};
+
+struct BMX160BusStats {
+    uint32_t read_transactions;
+    uint32_t write_transactions;
+    uint32_t read_bytes;
+    uint32_t write_bytes;
+    uint64_t bus_time_us;
+};
+
+class BMX160Bosch {
+public:
+    static constexpr uint16_t FIFO_CAPACITY_BYTES = 1024;
+    static constexpr uint8_t MAX_FIFO_SAMPLES = 48;
+
+    explicit BMX160Bosch(TWIM *i2c);
+
+    bool init();
+    int start(uint8_t odr, float sample_rate_hz, uint8_t buffered_samples);
+    int stop();
+    int read(BMX160Sample *samples, uint8_t max_samples);
+
+    void resetBusStats();
+    BMX160BusStats getBusStats() const;
+
+private:
+    static BMX160Bosch *instance;
+
+    static int8_t bmiRead(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len);
+    static int8_t bmiWrite(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len);
+    static void bmiDelay(uint32_t period_ms);
+
+    static int8_t bmmRead(uint8_t reg_addr, uint8_t *data, uint32_t len, void *intf_ptr);
+    static int8_t bmmWrite(uint8_t reg_addr, const uint8_t *data, uint32_t len, void *intf_ptr);
+    static void bmmDelay(uint32_t period_us, void *intf_ptr);
+
+    int8_t busRead(uint8_t reg_addr, uint8_t *data, uint16_t len);
+    int8_t busWrite(uint8_t reg_addr, const uint8_t *data, uint16_t len);
+    int configureMagnetometer();
+    static uint8_t auxOdrFor(float sample_rate_hz);
+
+    TWIM *_i2c;
+    uint8_t _addr;
+    float _sample_rate_hz = 100.0f;
+
+    struct bmi160_dev _bmi = {};
+    struct bmm150_dev _bmm = {};
+    struct bmi160_fifo_frame _fifo = {};
+    struct bmm150_settings _bmm_settings = {};
+
+    uint8_t _fifo_data[FIFO_CAPACITY_BYTES] = {};
+    struct bmi160_sensor_data _accel[MAX_FIFO_SAMPLES] = {};
+    struct bmi160_sensor_data _gyro[MAX_FIFO_SAMPLES] = {};
+    struct bmi160_aux_data _aux[MAX_FIFO_SAMPLES] = {};
+
+    BMX160BusStats _stats = {};
+};
+
+#endif
