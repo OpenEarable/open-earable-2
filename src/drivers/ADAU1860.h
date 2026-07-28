8e23702340ef6c8a22c088da059da10cb7379e59
Oliver Bagge <bagge@teco.edu>
2026-07-28 09:57:50 +0200

    fix(audio): map DMIC gain channels and handle write failures

--- src/drivers/ADAU1860.h
+++ src/drivers/ADAU1860.h
@@ -413,7 +413,7 @@ public:
     uint8_t get_volume();
 
     // DMIC gain control
-    void mic_gain_write(uint8_t channel, uint8_t gain);
+    int mic_gain_write(uint8_t channel, uint8_t gain);
     uint8_t mic_gain_read(uint8_t channel);
 
 #if CONFIG_FDSP
@@ -421,7 +421,7 @@ public:
 #endif
 private:
     bool readReg(uint32_t reg, uint8_t * buffer, uint16_t len);
-    void writeReg(uint32_t reg, uint8_t * buffer, uint16_t len);
+    int writeReg(uint32_t reg, uint8_t * buffer, uint16_t len);
 
     int setup_EQ();
     int setup_FDSP();
